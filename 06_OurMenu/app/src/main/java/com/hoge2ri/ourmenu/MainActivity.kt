50dff85893db66cb0e68b5d7a0307ab3681f8563
Yuhei Matsuura <yuhei.m848@gmail.com>
2020-08-11 23:10:33 +0900

    6.4の途中

--- 06_OurMenu/app/src/main/java/com/hoge2ri/ourmenu/MainActivity.kt
+++ 06_OurMenu/app/src/main/java/com/hoge2ri/ourmenu/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerForContextMenu(menuImage)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -67,4 +68,5 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
