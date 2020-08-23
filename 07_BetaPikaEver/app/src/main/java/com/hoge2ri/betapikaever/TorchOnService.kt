8771d3932711694ce106e1fc6061c5577874acfc
gacha <gacha.applepie@gmail.com>
2020-08-23 23:15:01 +0900

    start with List7.6

---
+++ 07_BetaPikaEver/app/src/main/java/com/hoge2ri/betapikaever/TorchOnService.kt
@@ -0,0 +1,12 @@
+package com.hoge2ri.betapikaever
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TorchOnService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
